1d3efb1cefb9b539f6087fe01277ea5148e01fa4
VANBAO300149X <ctvxworld38@gmail.com>
2026-09-23 13:24:21 +0700

    Update and rename botsever_v79.py to botsever_v5.py

--- botsever_v5.py
+++ botsever_v5.py
@@ -97,7 +97,7 @@ class _AnsiStyle:
 Fore = _AnsiFore()
 Style = _AnsiStyle()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8843095617:AAHrVWM5zHvCpCaBH1A0wuQ5A7vQ7lViQ9Q").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8344831081:AAFefLGxnjypfXFZP5betG2uM71y6h_wui8").strip()
 POLL_TIMEOUT = 25
 MAX_TEXT = 3900
 FLUSH_INTERVAL = 0.40
@@ -3582,7 +3582,7 @@ def create_telegraph_page(key, device_id):
     
 # ================== KEY SERVER ==================
 KEY_SERVER_URL = os.getenv("KEY_SERVER_URL", "https://keyvip-sever1.onrender.com").rstrip("/")
-KEY_SERVER_ADMIN_SECRET = os.getenv("KEY_SERVER_ADMIN_SECRET", "").strip()
+KEY_SERVER_ADMIN_SECRET = os.getenv("KEY_SERVER_ADMIN_SECRET", "VBTOOL_VANBAO999").strip()
 
 # ================== HÀM MÃ HÓA ==================
 
